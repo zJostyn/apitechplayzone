34e257aee51bcfdb82ea88a250d1b5a86e51baed
zjostyn <jostyncruzquito@gmail.com>
2024-07-19 16:20:15 -0500

    Se modifico

--- src/database.ts
+++ src/database.ts
@@ -5,5 +5,8 @@ export const pool = new Pool({
     host:'dpg-cqdd0sbqf0us738fsgs0-a.oregon-postgres.render.com',
     password: 'O6zB6RPMaFc0CppUy2bIKCCTp4urGWhF',
     database: 'techplayzone',
-    port: 5432
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false // Esta configuración permite conexiones SSL sin verificar el certificado del servidor. Para mayor seguridad, debes proveer un certificado de servidor adecuado.
+      }
 })
