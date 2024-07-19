b4187943671ce32386c3c49ee2e2f91da7e1f080
zjostyn <jostyncruzquito@gmail.com>
2024-07-19 16:00:20 -0500

    Se arreglo el npm start

--- dist/database.js
+++ dist/database.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
 const pg_1 = require("pg");
 exports.pool = new pg_1.Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'jostyn001',
-    database: 'TechPlayZone',
+    user: 'techplayzone_user',
+    host: 'dpg-cqdd0sbqf0us738fsgs0-a.oregon-postgres.render.com',
+    password: 'O6zB6RPMaFc0CppUy2bIKCCTp4urGWhF',
+    database: 'techplayzone',
     port: 5432
 });
