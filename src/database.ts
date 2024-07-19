e7a2dd203890dd8e0a6b33fcf8f98b83a24dc671
zjostyn <jostyncruzquito@gmail.com>
2024-07-19 15:52:25 -0500

    Se modifico a la base externa

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,9 @@
 import {Pool} from 'pg';
 
 export const pool = new Pool({
-    user: 'postgres',
-    host:'localhost',
-    password: 'jostyn001',
-    database: 'TechPlayZone',
+    user: 'techplayzone_user',
+    host:'dpg-cqdd0sbqf0us738fsgs0-a.oregon-postgres.render.com',
+    password: 'O6zB6RPMaFc0CppUy2bIKCCTp4urGWhF',
+    database: 'techplayzone',
     port: 5432
 })
